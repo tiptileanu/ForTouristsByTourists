ccffc7991ef3e6a7186da5f4e03d9cb067dbe1e6
Daniel <tiptil@gmail.com>
2019-03-11 21:52:28 +0000

    the good one

---
+++ app/src/main/java/mic/alliwanna/be/fortouristsbytourists/ImagePickerActivity.java
@@ -0,0 +1,13 @@
+package mic.alliwanna.be.fortouristsbytourists;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_picker);
+    }
+}
