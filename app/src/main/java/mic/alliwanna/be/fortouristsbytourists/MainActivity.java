b0db53abaf5f428121758d6151a9175fca703bed
Daniel <tiptil@gmail.com>
2019-03-11 22:12:33 +0000

    added code on menu options: sign out and add poi

--- app/src/main/java/mic/alliwanna/be/fortouristsbytourists/MainActivity.java
+++ app/src/main/java/mic/alliwanna/be/fortouristsbytourists/MainActivity.java
@@ -1,14 +1,18 @@
 package mic.alliwanna.be.fortouristsbytourists;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int RC_SIGN_IN=1;
+    public static final int RC_SIGN_IN = 1;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
@@ -17,6 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                // sign out
+                AuthUI.getInstance().signOut(this);
+                return true;
+            case R.id.add_poi_menu:
+                // go to add poi activity
+                Intent intent = new Intent(this, ImagePickerActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     @Override
